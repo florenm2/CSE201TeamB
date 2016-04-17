c81b5afb90bccd62524980ae6132dbf40abb302a
benjamam <benjamam@miamioh.edu>
2016-04-17 16:27:35 -0400

    added method to display courses once

--- StartingChanges/Controller.java
+++ StartingChanges/Controller.java
@@ -8,23 +8,35 @@ public class Controller {
 	public static void main(String[] args) {
 		
 		ArrayList<Course> allCourses = new ArrayList<Course>();
+		ArrayList<Course> coursesOnce = new ArrayList<Course>();
 		try {
 			allCourses = ImportCSV.csvFileIN();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		/*
 		for(Course c : allCourses){
 			System.out.println(c.displayCourse());
 		}
+		*/
+		coursesOnce = displayCoursesOnce(allCourses);
+		
 	}
 	
-	public ArrayList<Course> displayCoursesOnce(){
+	/*
+	 * Filters through all courses offered in the .csv file and displays each course only once (removes multiple sections)
+	 */
+	public static ArrayList<Course> displayCoursesOnce(ArrayList<Course> allClasses){
 		ArrayList<Course> coursesOnce = new ArrayList<Course>();
-		
-		
-		
+		String prev = "";
+		for(Course all : allClasses){
+			if(!all.getCourseNum().equals(prev)){
+				coursesOnce.add(all);
+				System.out.println("CSE"+all.getCourseNum() + " " + all.getTitle());
+				prev = all.getCourseNum();
+			}
+		}		
 		return coursesOnce;
 	}
 	
