2f317f34900b0cd2442b80b41634507ca00c813a
benjamam <benjamam@miamioh.edu>
2016-05-01 17:06:27 -0400

    added displayFinalExams class

---
+++ displayFinalExams.java
@@ -0,0 +1,298 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.font.TextAttribute;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+
+
+public class displayFinalExams extends JFrame{
+
+	private int numDays = 6;
+	
+	public displayFinalExams(ArrayList<Course> coursesScheduled){
+		frameSetup(coursesScheduled);
+		
+		setVisible(true);
+	}
+	
+	
+	private void frameSetup(ArrayList<Course> coursesScheduled) {
+		setLayout(new BorderLayout());
+		setBounds(0, 0, 700, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		finalExamSetup(coursesScheduled);
+		buttonPanelSetup();
+		heading();
+		setVisible(true);
+		
+	}
+	
+	private void finalExamSetup(ArrayList<Course> coursesScheduled){
+		
+		JPanel days = new JPanel(new GridLayout(0,numDays));
+		
+		days.add(times());
+		days.add(monday(coursesScheduled));
+		days.add(tuesday());
+		days.add(wednesday());
+		days.add(thursday());
+		days.add(friday());
+		
+		add(days, BorderLayout.CENTER);
+	}
+	
+	private void buttonPanelSetup() {
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+		JButton back = new JButton("Back");
+		JButton save = new JButton("Save");
+		
+		buttonPanel.add(back);
+		buttonPanel.add(save);
+		
+		add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	private void heading(){
+		JPanel heading = new JPanel();
+		JLabel header = new JLabel("Final Exam Schedule");
+		header.setFont(header.getFont().deriveFont(Font.BOLD,18f));
+		heading.add(header);
+		
+		add(heading,BorderLayout.NORTH);
+	}
+	
+	private JPanel times(){
+		JPanel time = new JPanel(new GridLayout(13,0));
+		//sets border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		time.setBorder(blackline);
+		
+		//sets heading
+		JLabel heading = new JLabel("Exam Times");
+		time.add(heading);
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		//heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+		
+		
+		JLabel time1 = new JLabel("8:00-10:00am");
+		Font font1 = time1.getFont();
+		//time1.setFont(font.deriveFont(attributes));
+		JLabel course1 = new JLabel("");
+		JLabel time2 = new JLabel("10:15am-12:15pm");
+		//time2.setFont(font.deriveFont(attributes));
+		JLabel course2 = new JLabel("");
+		JLabel time3 = new JLabel("12:45-2:45pm");
+		//time3.setFont(font.deriveFont(attributes));
+		JLabel course3 = new JLabel("");
+		JLabel time4 = new JLabel("3:00-5:00pm");
+		//time4.setFont(font.deriveFont(attributes));
+		JLabel course4 = new JLabel("");
+		JLabel time5 = new JLabel("5:30-7:30pm");
+		//time5.setFont(font.deriveFont(attributes));
+		JLabel course5 = new JLabel("");
+		JLabel time6 = new JLabel("7:45-9:45pm");
+		//time6.setFont(font.deriveFont(attributes));
+		JLabel course6 = new JLabel("");
+		time.add(time1);
+		time.add(course1);
+		time.add(time2);
+		time.add(course2);
+		time.add(time3);
+		time.add(course3);
+		time.add(time4);
+		time.add(course4);
+		time.add(time5);
+		time.add(course5);
+		time.add(time6);
+		time.add(course6);
+		
+		
+	
+		return time;
+	}
+	
+	private JPanel monday(ArrayList<Course>coursesScheduled){
+		JPanel mon = new JPanel(new GridLayout(13,0));
+	
+		
+		//sets border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		mon.setBorder(blackline);
+		
+		//sets heading
+		JLabel heading = new JLabel("Monday");
+		mon.add(heading);
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+
+		
+		//8-10
+		JLabel time1 = new JLabel("");
+		JLabel time11 = new JLabel("");
+		//10:15-12:15
+		JLabel time2 = new JLabel("");
+		JLabel time22 = new JLabel("");
+		//12:45-2:45
+		JLabel time3 = new JLabel("");
+		JLabel time33 = new JLabel("");
+		//3-5
+		JLabel time4 = new JLabel("");
+		JLabel time44 = new JLabel("");
+		//5:30-7:30
+		JLabel time5 = new JLabel("");
+		JLabel time55 = new JLabel("");
+		//7:45-9:45
+		JLabel time6 = new JLabel("");
+		JLabel time66 = new JLabel("");
+		
+		for(Course scheduled: coursesScheduled){
+			//System.out.println("test");
+			if(scheduled.getStartTime() == 1300 &&
+					(scheduled.getDays().equals("MW") || 
+							scheduled.getDays().equals("MWF") ||
+							scheduled.getDays().equals("WF"))){
+				System.out.println("Success: " + scheduled.getCourseNum());
+				time3.setText(scheduled.getTitle());
+				validate();
+			}
+			else{
+				time3.setText("test");
+			}
+		}
+		
+		mon.add(time1);
+		mon.add(time11);
+		mon.add(time2);
+		mon.add(time22);
+		mon.add(time3);
+		mon.add(time33);
+		mon.add(time4);
+		mon.add(time44);
+		mon.add(time5);
+		mon.add(time55);
+		mon.add(time6);
+		mon.add(time66);
+		
+		
+		
+		return mon;
+	}
+	
+	private Component tuesday(){
+		JPanel tues = new JPanel();
+		
+		//set border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		tues.setBorder(blackline);
+		
+		//set heading
+		JLabel heading = new JLabel("Tuesday");
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+		tues.add(heading);
+		
+		return tues;
+	}
+	
+	private Component wednesday(){
+		JPanel wed = new JPanel();
+		
+		//set border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		wed.setBorder(blackline);
+		
+		//set heading
+		JLabel heading = new JLabel("Wednesday");
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+		wed.add(heading);
+		
+		return wed;
+	}
+	
+	private Component thursday(){
+		JPanel thur = new JPanel();
+		
+		//set border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		thur.setBorder(blackline);
+		
+		//set heading
+		JLabel heading = new JLabel("Thursday");
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+		thur.add(heading);
+		
+		return thur;
+	}
+	
+	private Component friday(){
+		JPanel fri = new JPanel();
+		
+		//set border
+		Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1);
+		fri.setBorder(blackline);
+		
+		//set heading
+		JLabel heading = new JLabel("Friday");
+		Font font = heading.getFont();
+		Map attributes = font.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		heading.setFont(font.deriveFont(attributes));
+		heading.setFont(heading.getFont().deriveFont(Font.BOLD,14f));
+		fri.add(heading);
+		
+		return fri;
+	}
+
+	public static void main(String[] args){
+		Course c381 = new Course();
+		c381.setCourseNum("381");
+		c381.setSubject("CSE");
+		c381.setDays("MW");
+		c381.setStartTime(1000);
+		c381.setEndTime(1100);
+
+		Course c464 = new Course();
+		c464.setCourseNum("464");
+		c464.setSubject("CSE");
+		c464.setDays("WF");
+		c464.setStartTime(1300);
+		c464.setEndTime(1350);
+		
+		ArrayList<Course> coursesScheduled = new ArrayList<Course>();
+		coursesScheduled.add(c381);
+		coursesScheduled.add(c464);
+		
+		new displayFinalExams(coursesScheduled);
+	}
+	
+	
+}
